f678231fa32f978d62d6fbf302fe3c253ec690ae
scott <scott@example.com>
2025-04-18 08:52:23 -0500

    oil

--- init.lua
+++ init.lua
@@ -15,35 +15,38 @@ keymap("v", "<space>x", ":.lua<CR>")
 keymap("n", "<M-j>", "<cmd>cnext<CR>")
 keymap("n", "<M-k>", "<cmd>cprev<CR>")
 keymap('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagnostics' })
+keymap("n", "-", "<cmd>Oil<CR>")
 
+--- highlight yanked text ---
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup('hmmm', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup('hmmm', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end
 })
 
+--- terminal ---
 vim.api.nvim_create_autocmd('TermOpen', {
-    group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
-    callback = function()
-        vim.opt.number = false
-        vim.opt.relativenumber = false
-    end,
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
 })
 
 local job_id = 0
 vim.keymap.set("n", "<space>st", function()
-    vim.cmd.vnew()
-    vim.cmd.term()
-    vim.cmd.wincmd("J")
-    vim.api.nvim_win_set_height(0, 10)
-    job_id = vim.bo.channel
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 10)
+  job_id = vim.bo.channel
 end)
 
 vim.keymap.set("n", "<space>example", function()
-    vim.fn.chansend(job_id, { "ls -al\r\n" })
-    --vim.cmd("normal G")
+  vim.fn.chansend(job_id, { "ls -al\r\n" })
+  --vim.cmd("normal G")
 end)
 
 
