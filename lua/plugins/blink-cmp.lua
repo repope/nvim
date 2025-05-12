712f6bc852306602100d14066c16138895f74e5b
scott <scott@example.com>
2025-05-11 20:41:42 -0500

    blink signature

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -31,12 +31,12 @@ return {
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = "mono",
+      nerd_font_variant = "normal",
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
-
+    completion = { documentation = { auto_show = true } },
+    signature = { enabled = true },
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
